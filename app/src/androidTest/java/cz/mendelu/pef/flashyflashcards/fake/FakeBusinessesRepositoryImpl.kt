bdff6eed893b5917a4056d8dc990f93e0a2adc78
Daniil Astapenko <astapenko.daniil.03@gmail.com>
2024-01-16 22:19:00 +0100

    Change fake repository methods

--- app/src/androidTest/java/cz/mendelu/pef/flashyflashcards/fake/FakeBusinessesRepositoryImpl.kt
+++ app/src/androidTest/java/cz/mendelu/pef/flashyflashcards/fake/FakeBusinessesRepositoryImpl.kt
@@ -6,7 +6,7 @@ import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
 
 class FakeBusinessesRepositoryImpl : BusinessesRepository {
-    private val businesses = mutableListOf(
+    private val businessEntities = mutableListOf(
         BusinessEntity(
             id = 0,
             remoteId = "remote 0",
@@ -53,23 +53,46 @@ class FakeBusinessesRepositoryImpl : BusinessesRepository {
 
     override fun getAllBusinesses(): Flow<List<BusinessEntity>> {
         return flow {
-            emit(businesses)
+            emit(businessEntities)
         }
     }
 
     override suspend fun getBusinessByRemoteId(businessRemoteId: String): BusinessEntity? {
-        return businesses.firstOrNull { it.remoteId == businessRemoteId }
+        return businessEntities.firstOrNull { it.remoteId == businessRemoteId }
     }
 
     override suspend fun addNewBusiness(businessEntity: BusinessEntity): Long? {
-        return null
+        if (businessEntity.id == null) {
+            businessEntity.id = businessEntities.size.toLong()
+        }
+        businessEntities.add(businessEntity)
+
+        return businessEntity.id
     }
 
     override suspend fun updateBusiness(businessEntity: BusinessEntity) {
-
+        val businessToUpdate = businessEntities.firstOrNull { it.id == businessEntity.id }
+        businessToUpdate?.let {
+            it.remoteId = businessEntity.remoteId
+            it.name = businessEntity.name
+            it.imageUrl = businessEntity.imageUrl
+            it.category = businessEntity.category
+            it.displayAddress = businessEntity.displayAddress
+            it.businessUrl = businessEntity.businessUrl
+            it.rating = businessEntity.rating
+            it.reviewCount = businessEntity.reviewCount
+            it.latitude = businessEntity.latitude
+            it.longitude = businessEntity.longitude
+            it.whenAdded = businessEntity.whenAdded
+        }
     }
 
     override suspend fun deleteBusiness(businessEntity: BusinessEntity) {
-
+        for (index in businessEntities.indices) {
+            if (businessEntities[index].id == businessEntity.id) {
+                businessEntities.removeAt(index)
+                break
+            }
+        }
     }
 }
